0ac5c4585ff345ad557ea08365e4ec5bc4b6897f
Sander Saares <saares@axinom.com>
2017-07-03 12:39:12 +0300

    Revert "Experimenting with buffered write"
    This reverts commit aaea0bf946a191a9142da6acaef572a794677349.

--- EchoService/Echo.ashx.cs
+++ EchoService/Echo.ashx.cs
@@ -8,7 +8,7 @@ namespace EchoService
     {
         public void ProcessRequest(HttpContext context)
         {
-            context.Response.BufferOutput = true;
+            context.Response.BufferOutput = false;
 
             context.Response.ContentType = "application/octet-stream";
             context.Response.AddHeader("Content-Length", context.Request.ContentLength.ToString());
@@ -42,6 +42,7 @@ namespace EchoService
 
                     Trace.WriteLine($"Wrote {writeBytes} bytes for a total of {totalWritten}.");
                 }
+                
             }
 
             Trace.WriteLine("Finished request processing.");
