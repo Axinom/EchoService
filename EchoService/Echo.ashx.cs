3ca560b78440b527bafc6edc4e25ebd9bb2509c9
Sander Saares <saares@axinom.com>
2017-07-03 10:17:54 +0300

    Initial commit

---
+++ EchoService/Echo.ashx.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+
+namespace EchoService
+{
+    public class Echo : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.BufferOutput = false;
+
+            context.Response.ContentType = "application/octet-stream";
+            context.Response.AddHeader("Content-Length", context.Request.ContentLength.ToString());
+
+            var inputStream = context.Request.GetBufferlessInputStream();
+            inputStream.CopyTo(context.Response.OutputStream);
+        }
+
+        public bool IsReusable => true;
+    }
+}
