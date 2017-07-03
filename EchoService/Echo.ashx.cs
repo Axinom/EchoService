d2f01565fb4ee64784c671bded265e8db0a1597b
Sander Saares <saares@axinom.com>
2017-07-03 11:42:01 +0300

    Added some tracing to help see where things get stuck

--- EchoService/Echo.ashx.cs
+++ EchoService/Echo.ashx.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System.Diagnostics;
+using System.Web;
 
 namespace EchoService
 {
@@ -11,10 +12,37 @@ namespace EchoService
             context.Response.ContentType = "application/octet-stream";
             context.Response.AddHeader("Content-Length", context.Request.ContentLength.ToString());
 
+            Trace.WriteLine($"Incoming request with body size {context.Request.ContentLength}.");
+
             var inputStream = context.Request.GetBufferlessInputStream();
-            inputStream.CopyTo(context.Response.OutputStream);
+
+            long totalRead = 0;
+            long totalWritten = 0;
+
+            const int bufferSize = 0x14000;
+
+            // This copying code is just Stream.CopyTo() with added tracing.
+            int numBytes;
+            byte[] buffer = new byte[bufferSize];
+            while ((numBytes = inputStream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                totalRead += numBytes;
+
+                Trace.WriteLine($"Read {numBytes} bytes for a total of {totalRead}.");
+
+                context.Response.OutputStream.Write(buffer, 0, numBytes);
+
+                totalWritten += numBytes;
+
+                Trace.WriteLine($"Wrote {numBytes} bytes for a total of {totalWritten}.");
+            }
+
+            Trace.WriteLine("Finished request processing.");
+
+            context.Response.Flush();
+            context.Response.Close();
         }
 
-        public bool IsReusable => true;
+        public bool IsReusable => false;
     }
 }
